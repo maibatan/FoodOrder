7d1eb700851257a260fb6ca0452f4286667f48b5
maibatan <mait88319@gmail.com>
2021-04-01 11:30:31 +0700

    Update SMS

--- app/src/main/java/com/example/foodorder/DetailsActivity.java
+++ app/src/main/java/com/example/foodorder/DetailsActivity.java
@@ -3,10 +3,9 @@ package com.example.foodorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -15,10 +14,14 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
     }
+
     public void onClick(View v)
     {
-        String url="https://www.google.com/maps/dir/"+"/Saigon+University,+273+An+D.+V%C6%B0%C6%A1ng,+Ph%C6%B0%E1%BB%9Dng+3,+Qu%E1%BA%ADn+5,+Th%C3%A0nh+ph%E1%BB%91+H%E1%BB%93+Ch%C3%AD+Minh+700000,+Vietnam";
-        Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        Intent intent =new Intent(getApplicationContext(), OrderActivity.class);
+        TextView amount=(TextView) findViewById(R.id.textView14);
+        TextView dishes=(TextView) findViewById(R.id.textView12);
+        intent.putExtra("amount",amount.getText());
+        intent.putExtra("dishes",dishes.getText());
         startActivity(intent);
     }
 
---
+++ app/src/main/java/com/example/foodorder/OrderActivity.java
@@ -0,0 +1,38 @@
+package com.example.foodorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Button btn=(Button) findViewById(R.id.order);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText numerphone =(EditText) findViewById(R.id.editTextNumber);
+                if(!numerphone.getText().equals("") )
+                {
+                    Bundle bundle=getIntent().getExtras();
+                    String amount=bundle.getString("amount");
+                    String dishes=bundle.getString("dishes");
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage("5556", null,
+                            "I want "+amount+" "+dishes+"\nMy numer phone is "+numerphone.getText(),
+
+                            null, null);
+
+                }
+            }
+        });
+    }
+
+
+}
